f92f88b8ced3eec297afb58c023d56780b33d5b0
c8112002 <c8112002@gmail.com>
2018-01-08 19:29:40 +0900

    add body-parser

--- api/app.js
+++ api/app.js
@@ -1,13 +1,13 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 // Constants
 const PORT = 8080
 const HOST = "0.0.0.0"
 
-// App
 const app = express()
 
-// routes
+app.use(bodyParser.json())
 app.use("/articles", require("./routes/article"))
 
 app.listen(PORT, HOST)
