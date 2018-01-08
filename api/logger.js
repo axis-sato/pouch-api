47f165a6fd13e2c3709925aa1801d7628a89c5c3
c8112002 <c8112002@gmail.com>
2018-01-08 17:43:20 +0900

    add dotenv

--- api/logger.js
+++ api/logger.js
@@ -1,19 +1,29 @@
 const bunyan = require("bunyan")
 const PrettyStream = require("bunyan-prettystream")
+require("dotenv").config()
 
-const prettyStdOut = new PrettyStream()
-prettyStdOut.pipe(process.stdout)
+const baseOpts = {
+  name: "pouch-api"
+}
 
-var logger = bunyan.createLogger({
-  name: "pouch-api",
-  src: true,
-  streams: [
-    {
-      level: "debug",
-      type: "raw",
-      stream: prettyStdOut
-    }
-  ]
-})
+const opts = env => {
+  if (env === "dev") {
+    const prettyStdOut = new PrettyStream()
+    prettyStdOut.pipe(process.stdout)
 
-module.exports = logger
+    return Object.assign(baseOpts, {
+      src: true,
+      streams: [
+        {
+          level: "debug",
+          type: "raw",
+          stream: prettyStdOut
+        }
+      ]
+    })
+  }
+
+  return Object.assign(baseOpts, {})
+}
+
+module.exports = bunyan.createLogger(opts(process.env.NODE_ENV))
