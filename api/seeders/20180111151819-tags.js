0398f383471788fec2ad166e5cfd8bbf7856b7cd
c8112002 <c8112002@gmail.com>
2018-01-12 00:38:32 +0900

    add tags seed

---
+++ api/seeders/20180111151819-tags.js
@@ -0,0 +1,21 @@
+const tableName = "tags"
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(tableName, [
+      {
+        name: "tag1"
+      },
+      {
+        name: "tag2"
+      },
+      {
+        name: "tag3"
+      }
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(tableName, null)
+  }
+}
