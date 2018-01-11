7bc73cc8de0db585250e9de479f47fda7a47ec68
c8112002 <c8112002@gmail.com>
2018-01-12 00:15:51 +0900

    add articles seed

---
+++ api/seeders/20180109140720-articles.js
@@ -0,0 +1,24 @@
+const tableName = "articles"
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(tableName, [
+      {
+        title: "title1",
+        url: "https://google.com"
+      },
+      {
+        title: "title2",
+        url: "https://facebook.com"
+      },
+      {
+        title: "title3",
+        url: "https://twitter.com"
+      }
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(tableName, null)
+  }
+}
