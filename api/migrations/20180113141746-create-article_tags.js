17a225f3fd8a5c19f09d8742df178243e1162042
c8112002 <c8112002@gmail.com>
2018-01-13 23:46:48 +0900

    add tag_article migration and seeder

---
+++ api/migrations/20180113141746-create-article_tags.js
@@ -0,0 +1,36 @@
+const tableName = "article_tags"
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(tableName, {
+      article_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "articles",
+          key: "id"
+        }
+      },
+      tag_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "tags",
+          key: "id"
+        }
+      },
+      created_at: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(tableName)
+  }
+}
---
+++ api/seeders/20180113142712-article_tags.js
@@ -0,0 +1,32 @@
+const tableName = "article_tags"
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(tableName, [
+      {
+        article_id: 1,
+        tag_id: 1
+      },
+      {
+        article_id: 1,
+        tag_id: 2
+      },
+      {
+        article_id: 1,
+        tag_id: 3
+      },
+      {
+        article_id: 2,
+        tag_id: 1
+      },
+      {
+        article_id: 2,
+        tag_id: 2
+      }
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(tableName, null)
+  }
+}
