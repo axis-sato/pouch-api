a92c07dc861a2798a8f1c4efa76fcda623582479
c8112002 <c8112002@gmail.com>
2018-01-14 00:21:39 +0900

    implement the article controller

--- api/routes/article.js
+++ api/routes/article.js
@@ -1,16 +1,27 @@
 const express = require("express")
 const router = express.Router()
+const models = require("../models")
+const logger = require("../logger")
+const sequelize = require("sequelize")
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "articles"
+router.get("/", async (req, res) => {
+  const articles = await models.sequelize.query("SELECT * FROM articles", {
+    type: sequelize.QueryTypes.SELECT
   })
+  res.json(articles)
 })
 
-router.get("/:id", (req, res) => {
-  res.json({
-    message: "article"
-  })
+router.get("/:id", async (req, res) => {
+  const id = req.params.id
+  const article = await models.sequelize.query(
+    "SELECT * FROM articles WHERE id = :id ",
+    {
+      replacements: { id: id },
+      type: sequelize.QueryTypes.SELECT
+    }
+  )
+
+  res.json(article)
 })
 
 module.exports = router
