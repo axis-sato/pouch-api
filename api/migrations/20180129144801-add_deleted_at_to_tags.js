565d3a27c0dadbe94ca103299dfe3aab17858fe3
Masahiko Sato <sato_m@mediaxis.jp>
2018-01-29 23:50:52 +0900

    add deleted_at column to articles, comments and tags table

---
+++ api/migrations/20180129144801-add_deleted_at_to_tags.js
@@ -0,0 +1,16 @@
+const tableName = "tags"
+const columnName = "deleted_at"
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(tableName, columnName, {
+      type: Sequelize.DATE,
+      defaultValue: null,
+      after: "updated_at"
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(tableName, columnName)
+  }
+}
