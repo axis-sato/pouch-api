7871be3ee7ee624132331c96dd94115a5ddda851
Masahiko Sato <sato_m@mediaxis.jp>
2018-02-01 23:34:19 +0900

    fix the comments table scheme

---
+++ api/migrations/20180201142730-drop_comments.js
@@ -0,0 +1,45 @@
+const tableName = "comments"
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(tableName)
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(tableName, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED
+      },
+      article_id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "articles",
+          key: "id"
+        }
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      created_at: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        defaultValue: null
+      }
+    })
+  }
+}
---
+++ api/migrations/20180201143022-create_comments.js
@@ -0,0 +1,31 @@
+const tableName = "comments"
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(tableName, {
+      article_id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: "articles",
+          key: "id"
+        }
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      updated_at: {
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(tableName)
+  }
+}
