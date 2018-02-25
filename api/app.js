f75da713dfa360fe54e2a90b6e0e0eb0113c7713
Masahiko Sato <sato_m@mediaxis.jp>
2018-02-25 23:50:30 +0900

    cors

--- api/app.js
+++ api/app.js
@@ -7,6 +7,15 @@ const HOST = '0.0.0.0'
 
 const app = express()
 
+// CORS対応
+require('./logger').debug(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    next()
+  })
+}
+
 app.use(bodyParser.json())
 
 const basePath = '/v1'
